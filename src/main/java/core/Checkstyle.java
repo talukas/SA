0a8d59aa8114aeef9776bf51f9e34ba3e4adf3bc
Tania <taluk@outlook.com>
2017-05-26 21:59:08 -0700

    correction 2

--- src/main/java/core/Checkstyle.java
+++ src/main/java/core/Checkstyle.java
@@ -1,9 +1,10 @@
 package core;
 // http://checkstyle.sourceforge.net/
+
 public class Checkstyle {
 
-	// LineLengthCheck
-	void MethodNameMethodNameMethodName() {
+  // LineLengthCheck
+  void methodNameMethodNameMethodName() {
     System.out.println("Schoolmaster amnd The classroom is anagram? - true");
   }
 
